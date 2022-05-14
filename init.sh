7dca5c42f3382d678a5f8c9972e64b7d013afdea
Alexandr Timofeev <tim31al@gmail.com>
2022-05-14 14:09:37 +0300

    readme

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ docker-compose exec -u app activity composer install
 docker-compose exec -u app activity php bin/console doctrine:database:create -q || true
 docker-compose exec -u app activity php bin/console doctrine:migrations:migrate -q
 docker-compose exec -u app activity php bin/console doctrine:fixtures:load -q --group=AppFixtures
-docker-compose exec -u app php bin/console lexik:jwt:generate-keypair
+docker-compose exec -u app activity php bin/console lexik:jwt:generate-keypair
 
 docker-compose exec -u app landing composer install
 docker-compose exec -u app landing php bin/console doctrine:database:create -q || true
